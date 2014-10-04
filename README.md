f4c7c605cb0c1351dea278e7b67b604f141bc38b
Mattias Geniar <m@ttias.be>
2014-10-04 15:06:23 +0200

    Initial commit. Needs more loving.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 bash-script-skeleton-template
 =============================
 
-A simple template that can serve as the base for future shell scripts. Contains proper handling of options, has a help function, logging capabilities, ... 
+A simple template that can serve as the base for future shell scripts. Contains proper handling of options, has a help function, logging capabilities, ...
+
+how to use
+==========
+
+There's no real "guide" here. Just have a look at "template.sh", copy the bits you like, remove the bits you don't like and use the template as a basis for your future scripts.
+
+The template uses no external sources (so no shFlags etc.) and can run on its own.
+
+credit where credit is due
+==========================
+
+This template was created with inspiration from a lot of other templates. Among which;
+- http://stackoverflow.com/questions/14008125/shell-script-common-template
+- https://github.com/hemantborole/skeleton/blob/master/starter.sh
---
+++ template.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+#
+#
+#--------------------------------------------------------------------
+
+# Create a few constants that can be used further in the script
+VERSION=0.1.0
+USAGE_INFO="Usage: command -h args"
+
+# Determine some basic information, used later in the script
+this_path=$(readlink -f $0)        ## Path of this file including filename
+dir_name=`dirname ${this_path}`    ## Dir where this file is
+myname=`basename ${this_path}`     ## file name of this script.
+logger="${myname}.log"
+
+# Create several routines for handling our "common" use cases: help, log, output, ...
+# Routine: display help usage to the user
+function help {
+  echo "
+  usage: $myname [options]
+
+  -h           optional  Print this help message
+  -q           optional  Suppress log messages on screen, just log them.
+  -f           optional  Force
+  -i <file>    required  input file
+  -l <log>     optional  print errors and output to this file.
+               default ${myname}.log
+  -o <outdir>  optional  store output here.
+               default is <put ur default here>"
+  exit 1
+}
+
+# Routine: log to a file
+function log {
+  current_timestamp=`date`
+  if [[ $quiet == "true" ]]
+  then
+    echo $current_timestamp ": " $1 1>> $logger
+  else
+    echo $current_timestamp ": " $1 | tee -a $logger
+  fi
+}
+
+log "Script called"
+
+# If no arguments are passed to this script, it should always show the usage info.
+# The script will never perform any action unless the necessary parameters are explicitly used.
+if [ $# == 0 ] ; then
+  # Show the help usage
+  help
+  exit 1;
+fi
+
+# Put your main code here.
+function main {
+  help
+}
+
+# Bootstrap the script. Nothing much to do here.
+main "$@"
